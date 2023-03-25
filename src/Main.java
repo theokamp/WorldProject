32d4b7dfb24b2fa03b62295a5c24ac321a54feff
TheoKamp <77067397+TheoKamp@users.noreply.github.com>
2023-03-25 10:21:17 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import javafx.application.*;
+import javafx.geometry.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+import javafx.scene.control.*;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+	    launch(args);
+    }
+}
